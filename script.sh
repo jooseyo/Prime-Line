f34db0b7426f9752c2037a923029f3e0f26304f0
toughshj <toughshj@gmail.com>
2015-11-17 00:43:24 +0900

    script comment edit

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-# script to run the prime program several times, in order not to run too long
+# script to run the prime program several times, in order not to run too long.
 for i in `seq 1 50`; do
 	./prime 10
 done
